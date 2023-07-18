50a8478363146144f51eed8b9820139c57171ea5
mikikazuo <ers7m3b@gmail.com>
2023-07-18 09:42:25 +0900

    【サイト】mapへのkey付与、twエラーの回避

--- precedent/app/opengraph-image.tsx
+++ precedent/app/opengraph-image.tsx
@@ -28,7 +28,7 @@ export default async function OG() {
         <img
           src={new URL("../public/logo.png", import.meta.url).toString()}
           alt="Precedent Logo"
-          tw="w-20 h-20 mb-4 opacity-95"
+          // tw="w-20 h-20 mb-4 opacity-95"
         />
         <h1
           style={{
--- precedent/app/page.tsx
+++ precedent/app/page.tsx
@@ -76,7 +76,7 @@ export default async function Home() {
       </h2>
       <div className="repeat my-10 grid w-full max-w-screen-xl animate-fade-up gap-y-10">
         {analysisWeek.map((info) => (
-          <Card info={info} />
+          <Card key={info.buy_type} info={info} />
         ))}
       </div>
       <h2
@@ -87,7 +87,7 @@ export default async function Home() {
       </h2>
       <div className="repeat my-10 grid w-full max-w-screen-xl animate-fade-up gap-y-10">
         {analysisMonth.map((info) => (
-          <Card info={info} />
+          <Card key={info.buy_type} info={info} />
         ))}
       </div>
       <div className="text-white">
